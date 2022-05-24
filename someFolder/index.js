b6fc0616905fe14596dac72f970397e0331ad7d1
Ihar Berasneu <igor.simatic@gmail.com>
2022-05-24 12:58:28 +0300

    normal commit

--- someFolder/index.js
+++ someFolder/index.js
@@ -1,17 +1,1 @@
-var foo1 = 1
-;(function () {
-  if (foo) {
-    console.log(this)
-  } else {
-    console.log('no')
-  }
-})()
-
-var foo = 1
-
-;(function () {
-  if (foo) {
-    var foo = 2
-  }
-  console.log(foo)
-})()
+console.log('hello')
